9c3788c3d5dd76288104564adf625d0d44a66498
pythonwork88 <110053172+pythonwork88@users.noreply.github.com>
2022-09-21 19:31:15 +0200

    change to level4

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express()
 
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema ({
 
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -44,44 +44,46 @@ app.post('/login', function(req, res){
 
   User.findOne({email: username}, function(err, foundUser){
     if (err){
-
       console.log(err);
-
-    }else{
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets.ejs")
-        }else{
-          console.log("password wrong");
-          res.render("/")
-        }
-      }else{
-        res.redirect("/")
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if (result === true){
+            res.redirect("secrets")
+          }else {
+            res.redirect("/")
+          }
+        })
       }
     }
   })
 
-})
-
+});
 app.get('/register', function(req, res){
   res.render("register.ejs");
 })
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  })
-  newUser.save(function(err){
-    if (err){
-      console.log(err);
 
-    }else{
-      res.redirect("/")
-    }
-  })
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    })
+    newUser.save(function(err){
+      if (err){
+        console.log(err);
 
-})
+      }else{
+        res.redirect("/")
+      }
+    });
+
+
+  });
+
+
+});
 app.get('/secrets', function(req, res){
   res.render("secrets.ejs");
 })
