ce6ef2f8a32fc41d7a2c4275837a53618bdeefac
pythonwork88 <110053172+pythonwork88@users.noreply.github.com>
2022-09-22 18:44:24 +0200

    level 5 passport module

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+//const bcrypt = require("bcrypt");
+//const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const app = express()
 
@@ -15,6 +18,16 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false,
+
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema ({
@@ -22,77 +35,119 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-
+userSchema.plugin(passportLocalMongoose);
 
 
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get('/', function(req, res){
   res.render("home.ejs");
 })
 
+app.post("/register", function(req, res){
+//<-- User.register comes from passport
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err){
+      console.log(err);
+      res.redirect("/register");
+    }else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets")
+      })
+    }
+  })
 
+
+  // bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+  //   const newUser = new User({
+  //     email: req.body.username,
+  //     password: hash
+  //   })
+  //   newUser.save(function(err){
+  //     if (err){
+  //       console.log(err);
+  //
+  //     }else{
+  //       res.redirect("/")
+  //     }
+  //   });
+  //
+  //
+  // });
+  //
+
+});
 
 app.get('/login', function(req, res){
   res.render("login.ejs");
 })
 
 app.post('/login', function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
     if (err){
       console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function(err, result){
-          if (result === true){
-            res.redirect("secrets")
-          }else {
-            res.redirect("/")
-          }
-        })
-      }
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets")
+      });
     }
-  })
+  });
+  // const username = req.body.username;
+  // const password = req.body.password;
+  //
+  // User.findOne({email: username}, function(err, foundUser){
+  //   if (err){
+  //     console.log(err);
+  //   } else {
+  //     if (foundUser) {
+  //       bcrypt.compare(password, foundUser.password, function(err, result){
+  //         if (result === true){
+  //           res.redirect("secrets")
+  //         }else {
+  //           res.redirect("/")
+  //         }
+  //       })
+  //     }
+  //   }
+  // })
 
 });
 app.get('/register', function(req, res){
   res.render("register.ejs");
 })
 
-app.post("/register", function(req, res){
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    })
-    newUser.save(function(err){
-      if (err){
-        console.log(err);
-
-      }else{
-        res.redirect("/")
-      }
-    });
-
-
-  });
-
-
-});
 app.get('/secrets', function(req, res){
-  res.render("secrets.ejs");
+  if (req.isAuthenticated()){
+    res.render("secrets.ejs");
+  } else {
+    res.redirect("/login")
+  }
 })
+
+app.get('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
 
 
 app.get('/submit', function(req, res){
   res.render("submit.ejs");
-})
-
+});
 
 
 
