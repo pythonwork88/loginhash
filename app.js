3cf4cfcfa999b37a3aa3bddb0e208930f6c8f774
pythonwork88 <110053172+pythonwork88@users.noreply.github.com>
2022-09-24 17:52:15 +0200

    last file

--- app.js
+++ app.js
@@ -258,10 +258,7 @@ app.post('/submit', function(req, res){
 })
 
 let port = process.env.PORT;
-if( port = null || port == "") {
+if (port == null || port == "") {
   port = 3000;
 }
-
-app.listen(port, function(){
-  console.log("Server started on port 3000");
-});
+app.listen(port);
